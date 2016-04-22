327d889d395908d41989bf7d6b857f4eb2e97fc4
inazo1115 <inaba1115@gmail.com>
2016-04-23 01:03:40 +0900

    toggle-theme

--- .emacs.d/inits/init-action.el
+++ .emacs.d/inits/init-action.el
@@ -1,2 +1,18 @@
-(load-theme 'misterioso t)
+;; theme
+(defun make-counter ()
+  (lexical-let ((x 0))
+    (lambda () (setq x (1+ x)))))
+
+(fset 'counter (make-counter))
+
+(defun toggle-theme ()
+  (interactive)
+  (let ((x (counter)))
+    (cond ((eq (% x 2) 0) (load-theme 'tsdh-light t))
+          ((eq (% x 2) 1) (load-theme 'tsdh-dark t))
+          (t "error"))))
+
+(toggle-theme)
+
+;; other
 (buffer-menu)
