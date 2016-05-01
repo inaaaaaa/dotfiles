dea982d9491dcb99799cf8a4778ad2e3257e5026
inazo <inaba1115@gmail.com>
2016-05-01 09:42:30 +0900

    Add skelton

--- .emacs.d/inits/init-action.el
+++ .emacs.d/inits/init-action.el
@@ -10,6 +10,15 @@
           ((eq (% x 2) 1) (load-theme 'tsdh-dark t))
           (t "error"))))
 
+;; skeleton-py
+(defun skeleton-py ()
+  (interactive)
+  (insert "#!/usr/bin/env python\n")
+  (insert "# -*- coding: utf-8 -*-\n")
+  (insert "\n")
+  (insert "if __name__ == '__main__':\n")
+  (insert "    print('hello')"))
+
 ;; main
 (fset 'counter (make-counter))
 (toggle-theme)
