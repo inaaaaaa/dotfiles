ab87ff1df5173235fea6de6ecc764810b3b0de13
inazo1115 <inaba1115@gmail.com>
2016-05-04 03:02:43 +0900

    Update

--- bin/g_push_i.sh
+++ bin/g_push_i.sh
@@ -2,4 +2,11 @@
 
 git branch
 branch=$(git branch | grep \* | awk '{print $2}')
+
+echo "You will push ${branch} branch. ok? [y/N]"
+read ans
+if [ "$ans" != "y" ]; then
+    exit 1
+fi
+
 git push origin $branch:$branch
