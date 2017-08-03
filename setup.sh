268eb7be016f207fb525fb1bbf47d8ba51212eb8
inazo1115 <inaba1115@gmail.com>
2017-08-04 00:16:47 +0900

    Fix

--- setup.sh
+++ setup.sh
@@ -15,14 +15,14 @@ else
     ln -is $(pwd)/bin      ~/bin
 fi
 
-echo ""
-echo "Please choose color of prompt."
-echo "  white, cyan, red or yellow. (default: white)"
+echo ''
+echo 'Please choose color of prompt.'
+echo '  white, cyan, red or yellow. (default: white)'
 read color
-case "$color" in
-    "cyan"   ) echo "You chose cyan."   ; color=36 ;;
-    "red"    ) echo "You chose red."    ; color=31 ;;
-    "yellow" ) echo "You chose yellow." ; color=33 ;;
-    *        ) echo "You chose white."  ; color=37 ;;
+case "${color}" in
+    'cyan'   ) echo 'You chose cyan.'   ; color=36 ;;
+    'red'    ) echo 'You chose red.'    ; color=31 ;;
+    'yellow' ) echo 'You chose yellow.' ; color=33 ;;
+    *        ) echo 'You chose white.'  ; color=37 ;;
 esac
 echo "export PS1=\"\[\033[1;${color}m\][\t]\u@\h:\w:\\\$?\[\033[0m\]\n$ \"" >> ~/.bashrc.local
