b7837a99af1d2d430272cd0f95a1457109beb7a3
inazo1115 <inaba1115@gmail.com>
2017-08-14 02:35:12 +0900

    Refactor

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,13 +1,61 @@
-;; Import build-in requirements
-(require 'cl)
+;; Package
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+(defvar packages-to-install
+  '(ac-haskell-process
+    ac-helm
+    ac-html
+    ac-html-bootstrap
+    ac-python
+    auto-complete
+    auto-install
+    clojure-mode
+    cmake-mode
+    csv-mode
+    erlang
+    f
+    flycheck
+    gitignore-mode
+    gnuplot-mode
+    go-mode
+    graphviz-dot-mode
+    haskell-mode
+    helm
+    jinja2-mode
+    js3-mode
+    json-mode
+    llvm-mode
+    markdown-mode
+    nginx-mode
+    package-lint
+    php-mode
+    python-mode
+    racket-mode
+    ruby-mode
+    s
+    scala-mode
+    ssh-config-mode
+    vimrc-mode
+    w3m
+    yaml-mode))
 
-;; Load init-files
-;; (package-initialize)
-(load "${HOME}/.emacs.d/inits/install-packages.el")
-(load "${HOME}/.emacs.d/inits/setup-package.el")
-(load "${HOME}/.emacs.d/inits/setting.el")
-(load "${HOME}/.emacs.d/inits/keybind.el")
-(load "${HOME}/.emacs.d/inits/init-action.el")
+(dolist (package packages-to-install)
+  (unless (package-installed-p package)
+    (package-refresh-contents)
+    (package-install package)))
+
+;; Settings
+(defconst pwd (file-name-directory load-file-name))
+(load (concat pwd "settings/setup-package.el"))
+(load (concat pwd "settings/setting.el"))
+(load (concat pwd "settings/keybind.el"))
+
+;; Init actions
+(load-theme 'tsdh-dark)
+(buffer-menu)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -15,7 +63,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yaml-mode w3m vimrc-mode ssh-config-mode racket-mode python-mode php-mode nginx-mode markdown-mode llvm-mode json-mode js3-mode jinja2-mode graphviz-dot-mode go-mode gnuplot-mode gitignore-mode csv-mode cmake-mode clojure-mode auto-install ac-python ac-html-bootstrap ac-html ac-helm ac-haskell-process))))
+    (yaml-mode w3m vimrc-mode ssh-config-mode scala-mode racket-mode python-mode php-mode package-lint nginx-mode markdown-mode llvm-mode json-mode js3-mode jinja2-mode graphviz-dot-mode go-mode gnuplot-mode gitignore-mode flycheck erlang csv-mode cmake-mode clojure-mode auto-install ac-python ac-html-bootstrap ac-html ac-helm ac-haskell-process))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
